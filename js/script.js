6cfb25718046a1fbbb5103bc26da0fdc58db0cae
Nitish Raj Uprety <uprety.nitish@gmail.com>
2019-04-02 23:52:48 +0545

    Home Page Done.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    $('.nav-item.dropdown').mouseenter(function() {
+        $(this).addClass('show');
+        $(this).children('.dropdown-menu').addClass('show');
+        $(this).children('.dropdown-toggle').attr('aria-expanded', 'true');
+    }).mouseleave(function() {
+        $(this).removeClass('show');
+        $(this).children('.dropdown-menu').removeClass('show');
+        $(this).children('.dropdown-toggle').attr('aria-expanded', 'false');
+    });
+
+});
